6fe1fdb37750fb76dd77ae67667d459f536e159b
Oxshady <Oxshady@github.com>
2023-12-27 06:49:51 -0500

    sending string to standard output using built-in printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: zero if success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
