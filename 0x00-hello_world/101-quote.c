70b19fae36970276600df0c5a7cc581452d0eb7c
Oxshady <Oxshady@github.com>
2023-12-27 18:45:14 -0500

    sending string to standard error using write  function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	const char quote[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2,quote,sizeof(quote)-1);
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, q, sizeof(q) - 1);
 	return (1);
 }
