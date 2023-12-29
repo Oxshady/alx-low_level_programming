f87c46df1944dcbdd90366fb32b5e8e60b8feb4d
Oxshady <Oxshady@github.com>
2023-12-29 14:59:33 -0500

    print combinators is done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: zero if success
+ */
+int main(void)
+{
+	int x = 0;
+
+	for (; x < 10; x++)
+	{
+		putchar('0' + x % 10);
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
