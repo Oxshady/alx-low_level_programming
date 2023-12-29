6622632e3b1a60ac874b9326c900eabc338ef80a
Oxshady <Oxshady@github.com>
2023-12-29 12:26:17 -0500

    check positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%lu is positive", n);
+		printf("%d is positive", n);
 	else if (n < 0)
-		printf("%lu is negative", n);
+		printf("%d is negative", n);
 	else
-		printf("%lu is zero", n);
+		printf("%d is zero", n);
 	return (0);
 }
