837cc7fdce8d2bc987d8620bcb1441f1151f997b
Oxshady <Oxshady@github.com>
2023-12-29 14:33:08 -0500

    printing digit with base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: zero if success
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+		printf("%i", num);
+	putchar('\n');
+	return (0);
+}
