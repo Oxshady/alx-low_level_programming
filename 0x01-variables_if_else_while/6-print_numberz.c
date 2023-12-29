7e2958ddf86d18bd0581440781cd0e9834e266a4
Oxshady <Oxshady@github.com>
2023-12-29 14:41:06 -0500

    printing digit with base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: zero if success
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+		putchar('0'+num % 10);
+	return (0);
+}
