28521d50ee17dae214dcd83ad8dd2a06f31832a7
Oxshady <Oxshady@github.com>
2024-01-02 14:54:47 -0500

    git the abs of an number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - get the abs of the number
+ * @i: takes an integer
+ * Return: the abs of i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (i * -1);
+	else
+		return (i);
+}
