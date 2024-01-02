38042a1fc1e526ae8fbd9c3f7eb1121ad961ea9c
Oxshady <Oxshady@github.com>
2024-01-02 14:04:57 -0500

    fuction that print alphabet chars 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing the alphabet chars 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int times, character;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (character = 97; character <= 122; character++)
+			_putchar(character);
+		putchar('\n');
+	}
+}
