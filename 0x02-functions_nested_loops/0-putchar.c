c0d700e646cf3d1c9dc7c255c6e69913d501eadc
Oxshady <Oxshady@github.com>
2024-01-02 13:43:52 -0500

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: zero if Success
+ * Description: print _putchar followed by newline
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
