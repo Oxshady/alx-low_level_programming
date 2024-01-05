8fb810262a1e8d3b85d673538bddca6988b0516f
Oxshady <Oxshady@github.com>
2024-01-05 13:29:42 -0500

    printing 9 times table is so easy

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - print times 9 table
+ * Return: void
+ */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if ((x * y) < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + (x * y));
+				if (y < 9)
+					_putchar(',')
+			}
+			if ((x * y) >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + ((x * y) / 10));
+				_putchar('0' + ((x * y) % 10));
+				if (y < 9)
+					_putchar(',');
+			}
+			if (y == 9)
+				_putchar('\n')
+		}
+	}
+}
