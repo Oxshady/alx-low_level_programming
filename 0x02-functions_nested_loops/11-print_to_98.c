d5f41768d9a5f9357e2e754136a3e8109e78164f
Oxshady <Oxshady@github.com>
2024-01-05 18:04:42 -0500

    printing all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,67 +6,42 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-		{
-			if (n < 10)
-			{
-				if (n < 0)
-				{
-					if (n < -9)
-					{
-						n *= -1;
-						_putchar('0' + (n / 10));
-						_putchar('0' + (n % 10));
-					}
-					else
-					{
-					n *= -1;
-					_putchar('-');
-					_putchar('0' + n);
-					}
-				}
-				else
-					_putchar('0' + n);
-			}
-			else 
-			{
-				_putchar('0' + (n / 10));
-				_putchar('0' + (n % 10));
-			}
-			if (n < 98)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
-		}
-	}
-	else if (n > 98)
+	if (n >= 98 && n < 100)
 	{
 		for (; n >= 98; n--)
 		{
-			if (n > 99)
-			{
-				_putchar('0' + ((n / 10) / 10));
-				_putchar('0' + ((n / 10) % 10));
-				_putchar('0' + (n % 10));
-				if (n > 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-			_putchar('0' + (n / 10));
-			_putchar('0' + (n % 10));
-			if (n > 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			}
+			_putchar('0' + (n / 10)), _putchar('0' + (n % 10));
 		}
 	}
+	else if (n > 99)
+	{
+		_putchar('0' + (n / 10) / 10), _putchar('0' + (n / 10) % 10);
+		_putchar('0' + (n % 10));
+	}
+	else if (n <= 98 && n >= 10)
+	{
+		_putchar('0' + (n / 10)), _putchar('0' + (n % 10));
+	}
+	else if (n >= 0 && n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else if (n < 0 && n > -10)
+	{
+		_putchar('-'), _putchar('0' + (n * -1);
+	}
+	else if (n < -9 && n > -100)
+	{
+		_putchar('-'), _putchar('0' + ((n * -1) / 10));
+		_putchar('0' + ((n * -1) % 10);
+	}
+	else if (n < 0 && n < -99)
+	{
+		_putchar('-'), _putchar('0' + (((n * -1) / 10) / 10));
+		_putchar('0' + (((n * -1) / 10) % 10)), _putchar('0' + ((n * -1) % 10);
+	}
+	if (n != 98)
+		_putchar(','), _putchar(' ');
+	else
+		_putchar('\n');
 }
