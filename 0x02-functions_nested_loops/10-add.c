517f0f412edd448ae2bfccc0687f436cda937620
Oxshady <Oxshady@github.com>
2024-01-05 13:44:51 -0500

    function that return the summation of two vars

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - calculate the sum of two variables
+ * @x: first integer
+ * @y: second integer
+ * Return: the sum of variables
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
