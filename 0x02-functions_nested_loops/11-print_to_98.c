1ade271ebad9e163577e4a95fe49edcbe212ebf8
Oxshady <Oxshady@github.com>
2024-01-07 09:15:09 -0500

    function to print numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,48 +1,30 @@
 #include <stdio.h>
-#include "main.h"
+#include <main.h>
 /**
- * print_to_98 - print all natural numbers from n to 98
- * @n: the start of the range
+ * print_to_98 - print numbers from n to 98 in order
+ * @n: the integer that we start from it to 98
  * Return: void
  */
 void print_to_98(int n)
 {
-	if (n >= 98 && n < 100)
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+				printf("%i\n", n);
+			else
+				printf("%i, ", n);
+		}
+	}
+	else if (n >= 98)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("hello");
+			if (n == 98)
+				printf("%i\n", n);
+			else
+				printf("%i, ", n);
 		}
 	}
-	else if (n > 99)
-	{
-		_putchar('0' + (n / 10) / 10), _putchar('0' + (n / 10) % 10);
-		_putchar('0' + (n % 10));
-	}
-	else if (n <= 98 && n >= 10)
-	{
-		_putchar('0' + (n / 10)), _putchar('0' + (n % 10));
-	}
-	else if (n >= 0 && n < 10)
-	{
-		_putchar('0' + n);
-	}
-	else if (n < 0 && n > -10)
-	{
-		_putchar('-'), _putchar('0' + (n * -1));
-	}
-	else if ((n < -9) && (n > -100))
-	{
-		_putchar('-'), _putchar('0' + ((n * -1) / 10));
-		_putchar('0' + ((n * -1) % 10));
-	}
-	else if (n < 0 && n < -99)
-	{
-		_putchar('-'), _putchar('0' + (((n * -1) / 10) / 10));
-		_putchar('0' + (((n * -1) / 10) % 10)), _putchar('0' + ((n * -1) % 10));
-	}
-	if (n == 98)
-		_putchar('\n');
-	else
-		_putchar(','), _putchar(' ');
 }
