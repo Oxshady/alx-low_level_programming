c14f3dd21abf69dd5faa440cecd5b7bfe23b1259
Oxshady <Oxshady@github.com>
2024-01-07 12:03:34 -0500

    printing the first 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	long int num1 = 1 , num2 = 2 , fibo;
+	for(size_t i = 0;i < 50; i++ )
+	{
+		fibo = num1 + num2;
+		num1 = num2;
+		num2 = fibo;
+		if(fibo < 0)
+		{
+			fibo *= -1;
+		}
+		printf("%li ",fibo);
+	}
+	return (0);
+}
