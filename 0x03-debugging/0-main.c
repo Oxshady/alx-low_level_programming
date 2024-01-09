2fb0bbff72d18d0e4321dedcf08f2389e670fafd
Oxshady <Oxshady>
2024-01-09 18:59:58 +0200

    testing positive or negative function in case 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - testing the positive or negative function in case 0
+ * Return: zero if success
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
