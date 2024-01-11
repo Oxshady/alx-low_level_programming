c1009c7e4ad938f6b3c95a15fc09d029c53b0b4b
Oxshady <Oxshady>
2024-01-12 00:21:59 +0200

    draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,15 @@
 void print_line(int n)
 {
 	int i = 0;
-
-	for (; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n > 0)
+		for (; i < n; i++)
 		{
 			_putchar('-');
 			if (i == (n - 1))
 				_putchar('\n');
 		}
-		else
-			_putchar('\n');
 	}
 }
