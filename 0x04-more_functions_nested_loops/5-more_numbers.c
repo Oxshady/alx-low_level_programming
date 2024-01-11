90c96366221bbaf7421b44e2e9521119b28500c2
Oxshady <Oxshady>
2024-01-11 23:28:11 +0200

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: void
+*/
+void more_numbers(void)
+{
+	int num, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num > 9)
+			{
+				_putchar('0' + (num / 10));
+				_putchar('0' + (num % 10));
+			}
+			else
+				_putchar('0' + num);
+			if ((num == 14) && (i != 9))
+				_putchar('\n');
+		}
+	}
+}
