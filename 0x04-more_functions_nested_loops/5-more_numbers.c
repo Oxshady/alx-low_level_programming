f575934b771536a8aac462215b78cb82c6aec01b
Oxshady <Oxshady>
2024-01-12 00:04:47 +0200

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,23 @@
 */
 void more_numbers(void)
 {
-	int num, i;
+	int num, i , firstn , secondn;
 
-	for (i = 0; i < 10; i++)
+	for(i = 0; i < 10; i++)
 	{
 		for (num = 0; num < 15; num++)
 		{
-			if (num > 9)
+			if (num < 10)
+				firstn = num;
+			else if (num > 9)
 			{
-				_putchar('0' + (num / 10));
-				_putchar('0' + (num % 10));
+				firstn = num / 10;
+				secondn = num % 10;
 			}
-			else
-				_putchar('0' + num);
-			if (num == 14)
+			_putchar('0' + firstn);
+			if (num > 9)
+				_putchar('0' + secondn);
+			if (num == 14 && i != 9)
 				_putchar('\n');
 		}
 	}
