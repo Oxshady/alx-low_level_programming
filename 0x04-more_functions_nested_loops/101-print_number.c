a338d9b6d61deaebc009f80702b4f2addd326f7d
Oxshady <Oxshady>
2024-01-12 04:22:18 +0200

    function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_number - print any integer value comes from parameter
+ * @n: integer to print
+*/
+void print_number(int n)
+{
+	if ((n >= 0) && (n < 10))
+		_putchar('0' + n);
+	else if ((n > 9) && (n < 100))
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+	else if ((n > 99) && (n < 1000))
+	{
+		_putchar('0' + ((n / 10) / 10));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
+	else if (n > 999)
+	{
+		_putchar('0' + (((n / 10) / 10) / 10));
+		_putchar('0' + (((n / 10) / 10) % 10));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
+	else if ((n < 0) && (n > -10))
+	{
+		n *= -1;
+		_putchar('-');
+		_putchar('0' + n);
+	}
+	else if ((n < -9))
+	{
+		n *= -1;
+		_putchar('-');
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+}
