5d61089fcd133d314e5a53cbe05287d4832a9cca
Oxshady <Oxshady>
2024-01-23 20:35:43 +0200

    pass by refrence to change the actual value of var

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to update the value of variable by passing it by refrence
+ * @n: is a pointer to variable of integer type
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
