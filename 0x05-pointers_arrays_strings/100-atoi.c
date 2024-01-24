965b07045f3a27ad083d4928f6754564c918434e
Oxshady <Oxshady>
2024-01-25 01:26:17 +0200

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include "main.h"
+/**
+ * _atoi -  convert a string to an integer
+ * @s: string to convert to int
+ * Return: return converted string to integer
+*/
 int _atoi(char *s)
 {
 	int i = 0, result = 0, digit = 0, sign = 1, x = 0;
-	for (; s[i] != '\0';i++)
+
+	for (; s[i] != '\0'; i++)
 	{
 		if ((s[i] >= 48 && s[i] <= 57))
 		{
@@ -11,11 +17,11 @@ int _atoi(char *s)
 			result = (result * 10) + digit;
 		}
 	}
-	for (;s[x] != 0; x++)
+	for (; s[x] != 0; x++)
 	{
 		if (s[x] == '-' && (s[x + 1] >= 48 && s[x + 1] <= 57))
 			sign = -1;
-		else if(s[x] == '+' && ((s[x + 1] >= 48 && s[x + 1] <= 57) || s[x + i] == " "))
+		else if (s[x] == '+' && (s[x + 1] >= 48 && s[x + 1] <= 57))
 			sign = 1;
 	}
 	result = result * sign;
