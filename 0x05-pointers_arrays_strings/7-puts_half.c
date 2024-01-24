b55dce52aed3540c51cd2dc299dd044b5ba2bffa
Oxshady <Oxshady>
2024-01-24 04:02:47 +0200

    print the second half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,7 @@ void puts_half(char *str)
 
 	while (*(str + i) != 0)
 		i++;
-	if (i % 2 == 0)
-		i /= 2;
-	else
-		i = ((i - 1) / 2);
+	i /= 2;
 	while (*(str + i) != 0)
 	{
 		_putchar(*(str + i));
