6fb4ba033cfa6d055b75d4aa6e2baa45fe1f96ad
Oxshady <Oxshady>
2024-01-24 05:06:14 +0200

    print the element of the array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,31 +15,23 @@ void print_array(int *a, int n)
 	for (; i < n; i++)
 	{
 		if (a[i] < 0)
-		{
-			a[i] *= -1;
-			_putchar('-');
-		}
+			a[i] *= -1, _putchar('-');
 		if (a[i] < 10)
-		{
 			_putchar('0' + n);
-		}
 		else if (a[i] > 9 && a[i] < 100)
 		{
-			_putchar('0' + (a[i] / 10));
-			_putchar('0' + (a[i] % 10));
+			_putchar('0' + (a[i] / 10)), _putchar('0' + (a[i] % 10));
 		}
 		else if (a[i] > 99 && a[i] < 1000)
 		{
 			_putchar('0' + (a[i] / 10) / 10);
-			_putchar('0' + (a[i] / 10) % 10);
-			_putchar('0' + (a[i] % 10));
+			_putchar('0' + (a[i] / 10) % 10), _putchar('0' + (a[i] % 10));
 		}
 		else if (a[i] > 999 && a[i] < 10000)
 		{
 			_putchar('0' + ((a[i] / 10) / 10) / 10);
 			_putchar('0' + ((a[i] / 10) / 10) % 10);
-			_putchar('0' + (a[i] / 10) % 10);
-			_putchar('0' + (a[i] % 10));
+			_putchar('0' + (a[i] / 10) % 10), _putchar('0' + (a[i] % 10));
 		}
 		if (a[i] == (a[n - 1]))
 			_putchar('\n');
