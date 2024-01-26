9d9a881610280a8ff85c1ed0f48e52684f43ffc1
Oxshady <Oxshady>
2024-01-26 06:24:16 +0200

    problem one on argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
+/**
+ * main - print the first command line arg which is program name
+ * @argc: the number of command line argument
+ * @argv: array of pointers to char (array of strings)
+ * Return: 0 if success
+*/
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s",argv[0]);
+	printf("%s", argv[0]);
+	return (0);
 }
