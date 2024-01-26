d851c98581236f79319697a27ad8d85dee8d1d8c
Oxshady <Oxshady@github.com>
2024-01-25 23:41:20 -0500

    print the number of command line argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the command line argument numbers
+ * @argc: the number of command line argument
+ * @argv: array of strings
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%i\n",(argc - 1));
+
+	return (0);
+}
