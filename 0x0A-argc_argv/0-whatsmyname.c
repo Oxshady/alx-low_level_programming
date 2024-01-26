3b88635eadec24af187302124b8c1dedeacdad42
Oxshady <Oxshady>
2024-01-26 06:27:10 +0200

    problem one on argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argv: array of pointers to char (array of strings)
  * Return: 0 if success
 */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int __attribute__((unused))argc, char *argv[])
 {
 	printf("%s", argv[0]);
-	
+
 	return (0);
 }
