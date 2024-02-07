5a6be9c398f002ab987e3856bb719d36a9764865
Oxshady <Oxshady>
2024-02-08 00:26:57 +0200

    creating function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,31 +3,25 @@
  * _strcmp - compare two strings lexicographically
  * @s1: first string
  * @s2: second string
- * Return: 15 if s1 is greatest -15 if  s1 is smallest 0 if both are equal
+ * Return: 15 if s1 is greatest -15 if s1 is smallest 0 if both are equal
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, result = 0, x1 = 0, x2 = 0;
+	int i = 0, result;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	if (*s1 == '\0')
+		return (-15);
+	if (*s2 == '\0')
+		return (15);
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
+		if (s1[i] == '\0' && s2[i] != '\0')
+			return (*s1 - *s2);
+		if (s2[i] == '\0' && s1[i] != '\0')
+			return (*s1 - *s2);
 		if (s1[i] == s2[i])
 			result = 0;
-		else if (s1[i] > s2[i])
-			result = 15;
-		else if (s1[i] < s2[i])
-			result = -15;
 		i++;
 	}
-	while (s1[x1] != '\0')
-		x1++;
-	while (s2[x2] != '\0')
-		x2++;
-	if (x1 > x2)
-		result = 15;
-	if (x1 < x2)
-		result = -15;
-	if (x1 == x2)
-		result = 0;
 	return (result);
 }
