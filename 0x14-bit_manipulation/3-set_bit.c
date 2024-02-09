ed401c5de6a07af64cd17dd69b0ee56a9a24fa04
Oxshady <Oxshady>
2024-02-09 23:37:41 +0200

    adding the prototype of set_bit function

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,13 +7,11 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	unsigned long int mask = 1UL << index;
-
-	int previous_bit = (*n >> index) & 1;
-
+	mask = 1UL << index;
 	*n |= mask;
-
-	return (previous_bit);
+	return (1);
 }
