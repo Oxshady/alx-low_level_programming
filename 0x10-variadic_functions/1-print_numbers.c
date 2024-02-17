b61f6066d9d83992d80d0015e9b4bff6a025870a
Oxshady <Oxshady>
2024-02-17 16:07:51 +0200

    adding the prototype of print_numbers functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - variadic function that prints numbers followed bya new line
+ * @separator: the string that represent separator between numbers
+ * @n: the numbers of arguments passed to variadic function
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(ap, signed int));
+		if (*separator && (i + 1 != n))
+			printf("%s", separator);
+	}
+	_putchar('\n');
+}
