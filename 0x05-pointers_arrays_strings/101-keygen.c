5110b3b8264c9cc5d5bfd70dba3ca3e7091fa95a
Oxshady <shadymahmod55@gmail.com>
2024-03-18 06:36:42 +0200

    crack password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 /**
- * checksum - check the sum of ascii of string
- * @str: string to calc it's ascii sum
- * Return: the value of ascii summation
-*/
+ * checksum - calculate the sum of ASCII values of a string
+ * @str: string to calculate its ASCII sum
+ * Return: the sum of ASCII values
+ */
 int checksum(char *str)
 {
-	int sum = 0;
-	int i;
+	int sum = 0, i = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -17,26 +17,47 @@ int checksum(char *str)
 	}
 	return (sum);
 }
+
 /**
- * main - fuction that generate rand pass for crack me prog
+ * main - function that generates a random password for the crackme program
  * Return: zero if success
-*/
+ */
 int main(void)
 {
 	char pass[35];
-	int i = 0, x = 0;
+	int i = 0, x = 0, j = 0;
 
 	srand(time(NULL));
 	do {
 		pass[i] = rand() % 94 + 33;
 		i++;
-	} while (checksum(pass) <= 2670 && i < 33);
-	x = 2772 - checksum(pass);
-	if (x >= 33 && x <= 126)
+	} while (checksum(pass) < 2772 && i < 35);
+
+	if (checksum(pass) < 2772)
 	{
-		pass[i] = x;
-		i++;
+		x = 2772 - checksum(pass);
+		if (x >= 33 && x <= 126)
+		{
+			pass[i] = x;
+			i++;
+			pass[i] = '\0';
+		}
 	}
-	pass[i] = '\0';
+	else if (checksum(pass) > 2772)
+	{
+		while (checksum(pass) != 2772)
+		{
+			pass[j]--;
+			if (pass[j] < 33)
+			{
+				pass[j] = 126;
+			}
+			j++;
+			if (pass[j] == '\0')
+			{
+				j = 0;
+			}
+		}
+	}
 	return (0);
 }
