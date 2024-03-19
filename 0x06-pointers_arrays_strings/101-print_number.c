78d5262633cf3b9313fd03e6d64f928d3dced5bd
Oxshady <shadymahmod55@gmail.com>
2024-03-19 02:48:11 +0200

    i try many methods but i found recursion is the best way to do it

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,44 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
+#include <limits.h>
 /**
  * print_number - function that print number
  * @n: number to print
-*/
+ */
+#include "main.h"
+
 void print_number(int n)
 {
-	if (n < 0)
-		n *= -1, _putchar('-');
-	if (n < 10 && n >= 0)
-		_putchar('0' + n);
-	else if (n < 100 && n >= 10)
-		_putchar('0' + n / 10), _putchar('0' + n % 10);
-	else if (n >= 100 && n < 1000)
+	if (n == INT_MIN)
 	{
-		_putchar('0' + (n / 10) / 10);
-		_putchar('0' + (n / 10) % 10), _putchar('0' + n % 10);
+		_putchar('-');
+		print_number(-(n / 10));
+		_putchar('0' + (-(n % 10)));
 	}
-	else if (n >= 1000 && n < 10000)
-	{_putchar('0' + ((n / 10) / 10) / 10);
-		_putchar('0' + ((n / 10) / 10) % 10);
-		_putchar('0' + (n / 10) % 10), _putchar('0' + n % 10);
-	}
-	else if (n >= 10000 && n < 100000)
-	{_putchar('0' + (((n / 10) / 10) / 10) / 10);
-		_putchar('0' + (((n / 10) / 10) / 10) % 10);
-		_putchar('0' + ((n / 10) / 10) % 10);
-		_putchar('0' + (n / 10) % 10), _putchar('0' + n % 10);
-	}
-	else if (n > 999999999)
+	else
 	{
-		_putchar('0' + (((n / 10) / 10) / 10) / 10 / 10 / 10 / 10 / 10 / 10);
-		_putchar('0' + (((n / 10) / 10) / 10) / 10 / 10 / 10 / 10 / 10 % 10);
-		_putchar('0' + (((n / 10) / 10) / 10) / 10 / 10 / 10 / 10 % 10);
-		_putchar('0' + ((((((n / 10) / 10) / 10) / 10) / 10) / 10) % 10);
-		_putchar('0' + (((((n / 10) / 10) / 10) / 10) / 10) % 10);
-		_putchar('0' + ((((n / 10) / 10) / 10) / 10) % 10);
-		_putchar('0' + (((n / 10) / 10) / 10) % 10);
-		_putchar('0' + (((n / 10) / 10) % 10));
-		_putchar('0' + (((n / 10) % 10)));
-		_putchar('0' + (n % 10));
+		if (n < 0)
+		{
+			n *= -1;
+			_putchar('-');
+		}
+		if (n < 10)
+			_putchar('0' + n);
+		else
+		{
+			print_number(n / 10);
+			_putchar('0' + (n % 10));
+		}
 	}
 }
