983a61d0b18b2ddee9da9ad29fdb93d421727e1d
Oxshady <shadymahmod55@gmail.com>
2024-03-22 01:34:32 +0200

    returns the nth node of a dlistint_t linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: address of the first node in the list
+ * @index: the index of node we want
+ * Return: the address of the node or NULL if failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node = head;
+	int i = 0;
+
+	if (!head)
+		return (NULL);
+	while (i <= index)
+	{
+		if (i == index)
+		{
+			return (node);
+		}
+		else
+		{
+			node = node->next;
+			i++;
+		}
+	}
+	return (NULL);
+}
--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -94,9 +94,33 @@
 //     print_dlistint(head);
 //     return (EXIT_SUCCESS);
 // }
+// int main(void)
+// {
+//     dlistint_t *head;
+
+//     head = NULL;
+//     add_dnodeint_end(&head, 0);
+//     add_dnodeint_end(&head, 1);
+//     add_dnodeint_end(&head, 2);
+//     add_dnodeint_end(&head, 3);
+//     add_dnodeint_end(&head, 4);
+//     add_dnodeint_end(&head, 98);
+//     add_dnodeint_end(&head, 402);
+//     add_dnodeint_end(&head, 1024);
+//     print_dlistint(head);
+//     free_dlistint(head);
+//     head = NULL;
+//     return (EXIT_SUCCESS);
+// }
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
     dlistint_t *head;
+    dlistint_t *node;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -108,6 +132,8 @@ int main(void)
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
+    node = get_dnodeint_at_index(head, 5);
+    printf("%d\n", node->n);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
