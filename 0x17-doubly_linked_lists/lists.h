62fabedc0916df739e9265faab05690a85f7e068
Oxshady <shadymahmod55@gmail.com>
2024-03-22 00:37:18 +0200

    doubly linked list is a great way to structure data

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,5 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct dlistint
+{
+	int n;
+	struct dlistint *prev;
+	struct dlistint *next;
+} dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 #endif
