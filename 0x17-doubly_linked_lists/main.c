e27bf1a0cc6108c926d0b7cde11955fd28b7f72a
Oxshady <shadymahmod55@gmail.com>
2024-03-22 02:09:56 +0200

    all test cases

--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -143,10 +143,29 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
+// int main(void)
+// {
+//     dlistint_t *head;
+//     int sum;
+
+//     head = NULL;
+//     add_dnodeint_end(&head, 0);
+//     add_dnodeint_end(&head, 1);
+//     add_dnodeint_end(&head, 2);
+//     add_dnodeint_end(&head, 3);
+//     add_dnodeint_end(&head, 4);
+//     add_dnodeint_end(&head, 98);
+//     add_dnodeint_end(&head, 402);
+//     add_dnodeint_end(&head, 1024);
+//     sum = sum_dlistint(head);
+//     printf("sum = %d\n", sum);
+//     free_dlistint(head);
+//     head = NULL;
+//     return (EXIT_SUCCESS);
+// }
 int main(void)
 {
     dlistint_t *head;
-    int sum;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -157,9 +176,48 @@ int main(void)
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
-    sum = sum_dlistint(head);
-    printf("sum = %d\n", sum);
-    free_dlistint(head);
-    head = NULL;
-    return (EXIT_SUCCESS);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 5);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    return (0);
 }
