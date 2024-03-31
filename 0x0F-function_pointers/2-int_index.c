e0e2285d3eb79aea38460988d40dedddd341dcb1
Oxshady <shadymahmod55@gmail.com>
2024-03-31 19:12:39 +0200

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,6 +2,7 @@
 /**
  * int_index - function that searches for an integer
  * @array: array of integers to search on it
+ * @size: size of array
  * @cmp: pointer to function that take int and return int
  * Return: index of element if found, -1 if not found
 */
