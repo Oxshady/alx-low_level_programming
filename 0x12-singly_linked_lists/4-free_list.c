5666a2b2c005baef3bcbd446a036caeead84a101
Oxshady <shadymahmod55@gmail.com>
2024-04-30 04:46:32 +0300

    done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - function that free the list
+ * @head: pointer to first node
+*/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+		}
+		head = NULL;
+	}
+}
